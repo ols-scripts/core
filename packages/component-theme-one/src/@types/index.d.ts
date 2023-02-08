b6d14192cd9831557639933aba7bb4356cc4216f
WisestCoder <dushao1314@foxmail.com>
2023-02-08 15:47:42 +0800

    chore: Remove component-theme-one

--- packages/component-theme-one/src/@types/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.less' {
-  const resource: { readonly [key: string]: string }
-  export default resource
-}
--- packages/component-theme-one/src/ReactLive/components/LiveError.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export default function LiveError(props) {
-  const { error } = props
-  return error ? <pre>{error}</pre> : null
-}
